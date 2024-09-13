aa89e5614041bc505f9e3b1d39686d0f7618a300
Jonas Echterhoff <jonas@unity3d.com>
2024-09-13 17:03:33 +0200

    handle signals

--- generate_command.sh
+++ generate_command.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+trap signal_handler SIGINT SIGTERM
+
+signal_handler() {
+    run
+}
+
 # Function to generate a custom command
 generate_command() {
     /Users/jechter/Shelper/Shelper/Shelper/bin/Debug/net8.0/Shelper
@@ -9,7 +15,11 @@ generate_command() {
     eval $CUSTOM_PROMPT
 }
 
-while true; do
-# Call the function to generate and run the custom command
-    generate_command
-done
+run() {
+	while true; do
+	# Call the function to generate and run the custom command
+		generate_command
+	done
+}
+
+run
