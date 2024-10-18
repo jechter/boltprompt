ca670d212bf20972b4210449c0646c98386fff71
Jonas Echterhoff <jonas@unity3d.com>
2024-10-18 17:06:43 +0200

    descriptions for config

--- boltprompt/Configuration.cs
+++ boltprompt/Configuration.cs
@@ -1,20 +1,28 @@
 using System.Drawing;
 using System.Reflection;
 using System.Text.Json;
+using LanguageModels;
 
 namespace boltprompt;
 
 internal class Configuration
 {
+    [DescriptionForLanguageModel("The maximum number of suggestions to show.")]
     public int NumSuggestions { get; set; } = 10;
+    [DescriptionForLanguageModel("The background color for suggestions (In rrggbb hex format)")]
     public string SuggestionBackgroundColor { get; set; } = "d0d0d0";
+    [DescriptionForLanguageModel("The background color for the selected suggestion (In rrggbb hex format).")]
     public string SelectedSuggestionBackgroundColor { get; set; } = "30c0ff";
+    [DescriptionForLanguageModel("The text color for suggestions (In rrggbb hex format).")]
     public string SuggestionTextColor { get; set; } = "303030";
+    [DescriptionForLanguageModel("The text color for the current proposed auto-complete selection in the command line (In rrggbb hex format).")]
     public string AutocompleteTextColor { get; set; } = "a0a0a0";
+    [DescriptionForLanguageModel("The prefix text to show before the command line prompt.")]
     public string PromptPrefix { get; set; } = Prompt.ComposePromptPrefixScheme([
         (BufferedConsole.ConsoleColor.Black, BufferedConsole.ConsoleColor.Gray20, "{user_name}"),
         (BufferedConsole.ConsoleColor.Gray6, BufferedConsole.ConsoleColor.Gray20, "{working_directory_short_path}{prompt_symbol}")
     ]);
+    [DescriptionForLanguageModel("Your OpenAI API key to use for AI-based suggestions.")]
     public string? OpenAiApiKey { get; set; } = null;
     
     private static Configuration Load()
@@ -33,7 +41,7 @@ internal class Configuration
     public static void ListProperties()
     {
         foreach (var pi in typeof(Configuration).GetProperties(BindingFlags.Instance | BindingFlags.Public))
-            Console.WriteLine(pi.Name);
+            Console.WriteLine($"{pi.Name}::{pi.CustomAttributes.FirstOrDefault(a => a.AttributeType == typeof(DescriptionForLanguageModel))?.ConstructorArguments.FirstOrDefault().Value}");
     }
     
     public string Get(string propertyName) => 
