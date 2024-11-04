a041d95affc2dfcc9afc0b82c3e340abb16b2fba
Jonas Echterhoff <jonas@unity3d.com>
2024-11-04 11:38:50 +0100

    linux paths

--- boltprompt/Paths.cs
+++ boltprompt/Paths.cs
@@ -1,15 +1,24 @@
 using System.Reflection;
+using System.Runtime.InteropServices;
 using NiceIO;
 
 namespace boltprompt;
 
 internal static class Paths
 {
-    private static NPath boltpromptUserDir { get; } = NPath.HomeDirectory.Combine("Library/Application Support/boltprompt").MakeAbsolute();
+    static Paths()
+    {
+        OSConfigDir = NPath.HomeDirectory.Combine(RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+            ? "Library/Application Support"
+            : ".config");
+    }
+    
+    private static NPath OSConfigDir { get; }
+    private static NPath boltpromptUserDir => OSConfigDir.Combine("boltprompt");
     public static NPath boltpromptSupportFilesDir { get; } = Assembly.GetExecutingAssembly().Location.ToNPath().ParentContaining("boltpromptSupportFiles").Combine("boltpromptSupportFiles");
-    public static NPath LogDir { get; } = boltpromptUserDir.Combine("Logs");
-    public static NPath GeneratedCommandsDir { get; } = boltpromptUserDir.Combine("Commands");
+    public static NPath LogDir => boltpromptUserDir.Combine("Logs");
+    public static NPath GeneratedCommandsDir => boltpromptUserDir.Combine("Commands");
     public static NPath BuiltInCommandsDir { get; } = boltpromptSupportFilesDir.Combine("Commands");
-    public static NPath History { get; } = boltpromptUserDir.Combine("history");
-    public static NPath Configuration { get; } = boltpromptUserDir.Combine("configuration");
+    public static NPath History => boltpromptUserDir.Combine("history");
+    public static NPath Configuration => boltpromptUserDir.Combine("configuration");
 }
