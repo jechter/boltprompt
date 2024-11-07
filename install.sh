f382a354aa628a6155715b0b4315abfbb18b9a4f
Jonas Echterhoff <jonas@unity3d.com>
2024-11-07 10:58:33 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+exit_if_tool_does_not_exist() {
+	if ! command -v "$1" &> /dev/null; then
+		echo "$1 could not be found."
+
+		if command -v brew &> /dev/null; then
+			read -p "Would you like to install $1 using brew? (y/n): " choice
+			if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+				brew install "$1"
+				if [ $? -ne 0 ]; then
+					echo "Failed to install $1."
+					exit 1
+				fi
+				echo "$1 has been installed."
+				return
+			else
+				echo "Please install $1 manually."
+				exit 1
+			fi
+		else
+			echo "Please install $1 manually."
+			exit 1
+		fi
+	fi
+}
+
+exit_if_tool_does_not_exist "git"
+exit_if_tool_does_not_exist "dotnet"
+
+
+oldpwd=$(pwd)
+boltprompt_src=$(mktemp -d $TMPDIR/boltprompt_src.XXXXXX)
+cd $boltprompt_src
+git clone --recurse-submodules https://github.com/jechter/boltprompt.git
+cd boltprompt
+dotnet publish --output ~/.boltprompt
+~/.boltprompt/boltprompt install
+
+cd $oldpwd
+rm -rf $boltprompt_src
