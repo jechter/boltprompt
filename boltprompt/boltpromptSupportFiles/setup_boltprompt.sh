6851e88ba3a0158152057b0731fcc2d59e29ed90
Jonas Echterhoff <jonas@unity3d.com>
2024-11-14 09:48:12 +0100

    don't run boltprompt when not running in a terminal

--- boltprompt/boltpromptSupportFiles/setup_boltprompt.sh
+++ boltprompt/boltpromptSupportFiles/setup_boltprompt.sh
@@ -1,53 +1,54 @@
 #!/usr/bin/env $SHELL   
 
-trap signal_handler SIGINT
-
-signal_handler() {
-    trap - SIGINT    
-}
-
-case "$SHELL" in
-  */bash)
-    SETUP_SCRIPT_PATH=$(readlink -f "${BASH_SOURCE[0]}")
-    ;;
-  */zsh)
-    # we don't want to print errors from "eval" if wildcards don't match files
-    setopt +o nomatch
-    SETUP_SCRIPT_PATH=$(readlink -f "${(%):-%N}")
-    ;;
-  *)
-    echo "Unsupported shell: $SHELL"
-    exit 1
-    ;;
-esac
-
-BOLTPROMPT_PATH=$(dirname "$(dirname "$SETUP_SCRIPT_PATH")")
-
-export PATH=$BOLTPROMPT_PATH:$PATH
-
-add_to_history() {
+if [ -n "$TERM" ]; then
+  trap signal_handler SIGINT
+  
+  signal_handler() {
+      trap - SIGINT    
+  }
+  
   case "$SHELL" in
     */bash)
-      history -s $1
+      SETUP_SCRIPT_PATH=$(readlink -f "${BASH_SOURCE[0]}")
       ;;
     */zsh)
-      print -s "$1"
+      # we don't want to print errors from "eval" if wildcards don't match files
+      setopt +o nomatch
+      SETUP_SCRIPT_PATH=$(readlink -f "${(%):-%N}")
       ;;
     *)
       echo "Unsupported shell: $SHELL"
       exit 1
       ;;
   esac
-}
-
-TEMP_RESULT=0
-
-restore_status() { return $TEMP_RESULT; }
-
-generate_command() {
+  
+  BOLTPROMPT_PATH=$(dirname "$(dirname "$SETUP_SCRIPT_PATH")")
+  
+  export PATH=$BOLTPROMPT_PATH:$PATH
+  
+  add_to_history() {
+    case "$SHELL" in
+      */bash)
+        history -s $1
+        ;;
+      */zsh)
+        print -s "$1"
+        ;;
+      *)
+        echo "Unsupported shell: $SHELL"
+        exit 1
+        ;;
+    esac
+  }
+  
+  TEMP_RESULT=0
+  
+  restore_status() { return $TEMP_RESULT; }
+  
+  generate_command() {
     boltprompt
     if [ $? -ne 0 ]; then
-      echo "boltprompt failed!"
+      echo "failed"
       return 1 
     else    
       CUSTOM_PROMPT=$(cat /tmp/custom-command)
@@ -58,12 +59,13 @@ generate_command() {
       add_to_history $CUSTOM_PROMPT
       return 0
     fi
-}
-
-run() {
-	while generate_command; do
-	  true
-	done
-}
-
-run
+  }
+  
+  run() {
+    while generate_command; do
+      true
+    done
+  }
+  
+  run
+fi
